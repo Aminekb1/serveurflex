1d36963646cee2d8a9226b557156d53b1a222566
Aminekb1 <amine.kbaier@esprit.tn>
2025-06-15 00:05:58 +0100

    ajout models controllers middleware

---
+++ Controllers/userController.js
@@ -0,0 +1,179 @@
+const userModel = require('../models/userModel')
+const bcrypt = require("bcrypt")
+module.exports.getAllUsers = async (req,res)=>{
+    try {
+        
+        const userList = await userModel.find()
+
+        res.status(200).json(userList)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+module.exports.addClient = async (req,res)=>{
+    try {
+        
+        const {nom, prenom,email,password,age}=req.body
+
+        const role = "client"
+
+        const newUser = new userModel({
+            nom, prenom,email,password,age,role
+        })
+
+        const useradded = await newUser.save()
+
+        res.status(200).json(useradded)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+module.exports.addAdmin = async (req,res)=>{
+    try {
+        
+        const {email,password}=req.body
+
+        const role = "admin"
+
+        const newUser = new userModel({
+            email,password,role
+        })
+
+        const useradded = await newUser.save()
+
+        res.status(200).json(useradded)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+module.exports.deletUserById = async (req,res)=>{
+    try {
+        const {id}=req.params
+        await userModel.findByIdAndDelete(id)
+
+        res.status(200).json("deleted")
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+module.exports.getUserById = async (req,res)=>{
+    try {
+        const {id}=req.params
+        const user = await userModel.findById(id).populate("cars").populate("hotels")
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+module.exports.getDataUserConnect = async (req,res)=>{
+    try {
+        const id=req.user._id //user connecte
+        const user = await userModel.findById(id).populate("cars").populate("hotels")
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+module.exports.updatePassword = async (req,res)=>{
+    try {
+        const {id}=req.params
+        const {password}=req.body
+        
+        const salt = await bcrypt.genSalt()
+        const hashPassword = await bcrypt.hash(password,salt)
+        
+        const user = await userModel.findByIdAndUpdate(id,{
+            $set: {password : hashPassword}
+        })
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+module.exports.updateUser = async (req,res)=>{
+    try {
+        const {id}=req.params
+        const {nom,prenom,age}=req.body
+        
+        await userModel.findByIdAndUpdate(id,{
+            $set: {nom,prenom,age}
+        })
+
+        const user = await userModel.findById(id)
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+module.exports.addUserWithImage = async (req, res) => {
+    try {
+      const userData = {
+        ...req.body,
+      };
+      if (req.file ) {
+        const { filename } = req.file;
+        console.log(filename);
+        userData.user_image = filename;
+      }
+      const user = new userModel(userData);
+      const addedusers = await user.save();
+  
+      res.status(200).json({ addedusers });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  const jwt = require("jsonwebtoken")
+
+  const createToken=(id) => {
+    return jwt.sign({id},'net 9antra25 secret',{expiresIn : '1m'})
+  }
+
+  
+
+  module.exports.login = async (req,res)=>{
+    try {
+        const { email , password} = req.body
+        
+        const user = await userModel.login(email,password)
+        const connecte = true
+        await userModel.findByIdAndUpdate(user._id,{
+            $set: {connecte}
+        })
+        const token = createToken(user._id)
+        res.cookie('jwt_token',token,{httpOnly:true,maxAge:60*1000})
+        res.status(200).json({message :"connected",user : user})
+    } catch (error) {
+        res.status(500).json({message:error.message} )
+    }
+  }
+
+
+
+  module.exports.logout = async (req,res)=>{
+    try {
+        const id = req.user_id 
+        
+        const connecte = false
+        await userModel.findByIdAndUpdate(id,{
+            $set: {connecte}
+        })
+        
+        res.cookie("jwt_token","",{httpOnly:false,maxAge:1})
+        res.status(200).json("User successfully logged out")
+    } catch (error) {
+        res.status(500).json({message:error.message} )
+    }
+  }
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/hello', function(req, res, next) {
+  res.status(200).json('marahbe bik');
 });
+
+/* GET users listing. */
+router.get('/getAllUsers',requireAuthUser,userController.getAllUsers );
+router.get('/getUserById/:id',userController.getUserById );
+router.post('/login',userController.login );
+router.post('/logout',requireAuthUser,userController.logout );
+router.post('/addClient',userController.addClient );
+router.post('/addAdmin',requireAuthUser,userController.addAdmin );
+router.post('/addUserWithImage',uploadfile.single("image_user"),userController.addUserWithImage );
+router.put('/updatePassword/:id',requireAuthUser,userController.updatePassword );
+router.put('/updateUser/:id',requireAuthUser,userController.updateUser );
+router.delete('/deletUserById/:id',userController.deletUserById );
 
 module.exports = router;
